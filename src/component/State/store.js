ee27ab8251a7d6cd75fe5977a8f967a6f4efd08d
Mansi87 <manupandit053@gmail.com>
2025-12-29 00:40:59 +0530

    updated store.jsfile

--- src/component/State/store.js
+++ src/component/State/store.js
@@ -1,6 +1,5 @@
-import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+import { configureStore } from '@reduxjs/toolkit';
 import { authReducer } from "./Authentication/Reducer";
-import { thunk } from "redux-thunk";
 import restaurantReducer from "./Restaurant/Reducer";
 import menuItemReducer from "./Menu/Reducer";
 import cartReducer from "./Cart/Reducer";
@@ -8,14 +7,14 @@ import { orderReducer } from "./Order/Reducer";
 import restaurantsOrderReducer from "./Restaurant Order/Reducer";
 import { ingredientReducer } from "./Ingredients/Reducer";
 
-const rooteReducer=combineReducers({
-    auth:authReducer,
-    restaurant:restaurantReducer,
-    menu:menuItemReducer,
-    cart:cartReducer,
-    order:orderReducer,
-    restaurantOrder:restaurantsOrderReducer,
-    ingredients:ingredientReducer
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    restaurant: restaurantReducer,
+    menu: menuItemReducer,
+    cart: cartReducer,
+    order: orderReducer,
+    restaurantOrder: restaurantsOrderReducer,
+    ingredients: ingredientReducer
+  }
 });
-
-export const store=legacy_createStore(rooteReducer,applyMiddleware(thunk))
